e40e267c93e535bd7b3c9205cf5aa42a55f171b0
JonathanBret <bret.jonathan@outlook.fr>
2026-02-05 00:15:35 +0100

    Schema user

---
+++ models/User.js
@@ -0,0 +1,11 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
