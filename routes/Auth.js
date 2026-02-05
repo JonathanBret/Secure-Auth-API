c1c64adc61cae8f1b79794b9f9f72fbf2f838f4a
JonathanBret <bret.jonathan@outlook.fr>
2026-02-06 00:43:01 +0100

    hasspassword

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/user.js"
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -9,5 +10,20 @@ router.post('/register', async (req, res) => {
 
   if (!email || !password) {
     return res.status(400).json({ message: "Email et mot de passe requis"});
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.status(400).json({ message: "Email déjà existant !"});
+  }
+  else {
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = new User ({
+        email : email,
+        password : hashPassword,
+    })
+    await newUser.save();
+    return res.status(201).json({ message: "L'utilisateur à été créer." })
   }
 })
