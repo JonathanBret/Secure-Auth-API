ea26d56e8453d6ee4417949b712b8f9cd3ea3621
JonathanBret <bret.jonathan@outlook.fr>
2026-02-06 17:26:31 +0100

    refresh token route with rotation and revocation

--- routes/Auth.js
+++ routes/Auth.js
@@ -52,6 +52,9 @@ router.post('/login', async (req, res) => {
             const userId = user._id;
             const accessToken = jwt.sign({ userId}, SECRET, { expiresIn: '15m'});
             const refreshToken = jwt.sign({ userId}, REFRESH_SECRET, { expiresIn: '7d'});
+            user.refreshTokens.push(refreshToken);
+            user.save();
+
 
             return res.json({ accessToken, refreshToken});
     });
@@ -64,12 +67,28 @@ router.post('/login', async (req, res) => {
         try {
         const payloadFreshToken = jwt.verify(freshToken, REFRESH_SECRET);
         const userId = payloadFreshToken.userId;
+
+        const user = await User.findOne({ _id: userId, refreshTokens: freshToken });
+        if (!user) {
+            return res.status(401).json({ message: "Token invalide ou révoqué." });
+        }
+
+        user.refreshTokens = user.refreshTokens.filter(token => token !== freshToken);
+
+        const newRefreshToken = jwt.sign({ userId }, REFRESH_SECRET, { expiresIn: '7d' });
+        user.refreshTokens.push(newRefreshToken);
+
+        await user.save();
+
         const newAccessToken = jwt.sign({ userId}, SECRET, { expiresIn: '15m'});
-        return res.json({ accessToken: newAccessToken});
+        
+        return res.json({ accessToken: newAccessToken, freshToken: newRefreshToken});
         }
         catch(err) {
             return res.status(401).json({ message: "Token invalide."});
         }
+
+
     })
 
     export default router;
